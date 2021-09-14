cdb6ab085b5b97fb5b6bbcfc402077bbd8bccd66
yerkortiz <yerko.ortizmora@gmail.com>
2021-09-14 10:06:48 -0300

    add: introduction 2 problems

---
+++ introduction/missing-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+#include <vector>
+using namespace std;
+
+int main() {
+  uint64_t n, 
+           sum = 0; 
+  cin>>n;
+  vector<int> v(n); 
+  for(auto i = 0; i < n - 1; ++i) {
+    cin>>v[i];
+    sum += v[i];
+  }
+  cout << (n*n + n)/2 - sum;
+  return 0;
+}
---
+++ introduction/weird-algorithm.cpp
@@ -0,0 +1,19 @@
+/*
+    Author: Yerko Ortiz
+    Task: https://cses.fi/problemset/task/1068/
+*/
+#include <iostream>
+using namespace std;
+int main() {
+  long long n; cin >> n;
+  while(n > 1) {
+    cout<<n<<' ';
+    if (n & 1) {
+      n = 3 * n + 1;
+    } else {
+      n = n / 2;
+    }
+  }
+  cout<<1;
+  return 0;
+}
